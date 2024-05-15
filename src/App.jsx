9f9e3d62a1d4b02b95a216ff6c7852f8a731e7ee
Whycodex <neerajgupta12300@gmail.com>
2024-05-15 23:39:32 +0530

    Setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>BreadCrumb</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
