67bda6642e6fe3170d24f7c851694d52ca499e37
Whycodex <neerajgupta12300@gmail.com>
2024-05-16 23:47:26 +0530

    Integrated api for products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        const slicedData = data.products.slice(0, 6);
+        setTrendingProducts(slicedData);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <span>Trending Products</span>
+      <div>
+        {trendingProducts?.map((product) => (
+          <div key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <img src={product.thumbnail} alt={product.title} />
+              <h3>{product.title}</h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <Link to={"/products"}>
+        <button style={{ width: "100%", padding: 10 }}>
+          View all products
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
